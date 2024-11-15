1807e2e4b0261580e8d1c03c5a21375e7b5ad2d4
Tu Nombre <th3L4sTa1.5.2@gmail.com>
2024-11-15 11:54:57 -0400

    Sexto commit

--- api/pasos/updatePaso.php
+++ api/pasos/updatePaso.php
@@ -3,24 +3,23 @@
 require_once('../../util/manejocore.php');
 require_once('../../config/conexion.php');
 
-// Obtener el ID del autor desde la URL
+// Obtener el ID del paso desde la URL
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
 // Obtener los datos JSON de la solicitud
 $data = json_decode(file_get_contents("php://input"));
 
-
-
 // Validar los datos
 if (isset($data->idcurso) && isset($data->idnivel) && isset($data->idtipo) && isset($data->paso)) {
     $idcurso = intval($data->idcurso); // Asegurarse que sea un entero
     $idnivel = intval($data->idnivel); // Asegurarse que sea un entero
-    $idtipo = intval($data->idtipo); // Cambiado a idtipo para ser consistente
+    $idtipo = intval($data->idtipo); // Asegurarse que sea un entero
     $paso = $data->paso; // Asumiendo que paso puede ser una cadena
 
-    // Preparar la consulta SQL para actualizar el autor
+    // Preparar la consulta SQL para actualizar el paso
     $sql = "UPDATE pasos SET idcurso = ?, idnivel = ?, idtipo = ?, paso = ? WHERE id = ?";
     $stmt = $conn->prepare($sql);
+    
     if ($stmt === false) {
         die(json_encode(["success" => false, "message" => "Error en la preparación de la consulta: " . $conn->error]));
     }
@@ -32,7 +31,7 @@ if (isset($data->idcurso) && isset($data->idnivel) && isset($data->idtipo) && is
     if ($stmt->execute()) {
         echo json_encode(["success" => true, "message" => "Paso actualizado exitosamente."]);
     } else {
-        echo json_encode(["success" => false, "message" => "Ocurrió un error al actualizar el autor: " . $stmt->error]);
+        echo json_encode(["success" => false, "message" => "Ocurrió un error al actualizar el paso: " . $stmt->error]);
     }
 
     $stmt->close();
