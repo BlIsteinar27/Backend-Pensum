d6e7e44637fdf4e3870c240ccb6f84503059a0f4
Tu Nombre <th3L4sTa1.5.2@gmail.com>
2024-11-25 11:58:33 -0400

    Noveno commit

--- api/pasos/postPaso.php
+++ api/pasos/postPaso.php
@@ -1,46 +1,40 @@
 <?php 
   
-
+// Conexión a la base de datos
 require_once('../../util/manejoCore.php');
 require_once('../../config/conexion.php');
 
-
-
-$idcurso    = $_POST['idcurso'];
-$idnivel    = $_POST['idnivel'];
-$idtipo  = $_POST['idtipo'];
-$paso  = $_POST['paso'];
-
+$idcurso    = htmlspecialchars(trim($_POST['idcurso']));
+$idnivel   = htmlspecialchars(trim($_POST['idnivel']));
+$idtipo  = htmlspecialchars(trim($_POST['idtipo']));
+$paso     = htmlspecialchars(trim($_POST['paso']));
 
 // Eliminar apóstrofes
-$paso = str_replace("'", "", $paso);
+$paso     = str_replace("'", "", $paso);
 
 // Decodificar entidades HTML
 $idcurso    = html_entity_decode($idcurso);
-$idnivel    = html_entity_decode($idnivel);
-$idtipo   = html_entity_decode($idtipo);
-$paso  = html_entity_decode($paso);
-
-
+$idnivel   = html_entity_decode($idnivel);
+$idtipo  = html_entity_decode($idtipo);
+$paso     = html_entity_decode($paso);
 
 // Preparar y ejecutar la consulta
-$sql = "INSERT INTO pasos (idcurso,idnivel, idtipo, paso) VALUES (?,?,?,?)";
+$sql = "INSERT INTO pasos (idcurso, idnivel, idtipo, paso) VALUES (?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-
 
 if ($stmt === false) {
     die(json_encode(["success" => false, "message" => "Error en la preparación de la consulta: " . $conn->error]));
-    }
-    
-$stmt->bind_param("iiis",$idcurso,$idnivel, $idtipo, $paso);
-    
+}
+
+$stmt->bind_param("iiis", $idcurso, $idnivel, $idtipo, $paso);
+
 if ($stmt->execute()) {
-        echo json_encode(["success" => false, "message" => "Nuevo paso agregado exitosamente."]);
-    } else {
-         error_log("Error al agregar paso: " . $stmt->error);
-        echo json_encode(["success" => false, "message" => "Ocurrió un error al agregar paso."]);
-     }
-           
+    echo json_encode(["success" => true, "message" => "Nuevo paso agregado exitosamente."]);
+} else {
+    error_log("Error al agregar paso: " . $stmt->error);
+    echo json_encode(["success" => false, "message" => "Ocurrió un error al agregar el paso."]);
+}
+
 // Cerrar conexión
 $stmt->close();
 $conn->close();
